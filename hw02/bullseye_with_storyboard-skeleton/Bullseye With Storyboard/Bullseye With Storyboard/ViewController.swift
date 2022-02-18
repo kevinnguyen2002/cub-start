978fda0e592c880f01fc4831069af115519be84b
Kevin Nguyen <kevinnguyen@berkeley.edu>
2022-02-17 20:10:24 -0800

    first commit

---
+++ hw02/bullseye_with_storyboard-skeleton/Bullseye With Storyboard/Bullseye With Storyboard/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  Storyboard Bullseye
+//
+//  Created by Tony Hong on 1/13/22.
+//
+
+import UIKit
+import Foundation
+
+var randomNumber = 0
+
+var range = 100
+
+var level = 1
+
+var highScoreValue = 0
+
+class ViewController: UIViewController {
+    
+    // switch that switches on and off from exact mode
+    @IBOutlet weak var exactSwitch: UISwitch!
+    
+    // displays whether user hit bullseye
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // displays the number user must hit to win
+    @IBOutlet weak var numLabel: UILabel!
+    
+    // displays the current level the user is on
+    @IBOutlet weak var currentLevel: UILabel!
+    
+    // displays high score of user
+    @IBOutlet weak var highScore: UILabel!
+    
+    // slider user can slide to match random number given
+    @IBOutlet weak var numSlider: UISlider!
+    
+    // IBOutlet for play again button
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    // displays the max value of slider
+    @IBOutlet weak var rangeLabel: UILabel!
+    
+    @IBAction func checkValue(_ sender: Any) {
+        if exactSwitch.isOn == false {
+            if randomNumber - 3 <= Int(numSlider.value) && randomNumber + 3 >= Int(numSlider.value) {
+                resultLabel.text = "You were right on point! Bullseye!"
+                range += 50
+                rangeLabel.text = String(range)
+                numSlider.maximumValue = Float(range)
+                
+                level += 1
+                currentLevel.text = String(level)
+                
+                numSlider.setValue(Float(range / 2), animated: false)
+                randomNumber = Int(arc4random_uniform(UInt32(range + 1)))
+                numLabel.text = String(randomNumber)
+                
+                resultLabel.isHidden = true
+            } else {
+                playAgainButton.isHidden = false
+                resultLabel.text = "Whoops! You missed! Try again later"
+            }
+        } else { //if exactSwitch.isOn is true
+            if exactSwitch.isOn == true {
+                resultLabel.text = "Bullseye"
+                
+                range += 50
+                rangeLabel.text = String(range)
+                numSlider.maximumValue = Float(range)
+                      
+                level += 1
+                currentLevel.text = String(level)
+                      
+                numSlider.setValue(Float(range / 2), animated: false)
+                randomNumber = Int(arc4random_uniform(UInt32(range + 1)))
+                numLabel.text = String(randomNumber)
+                resultLabel.isHidden = true
+            } else {
+                playAgainButton.isHidden = false
+                resultLabel.text = "You missed!"
+            }
+        }
+        resultLabel.isHidden = false //we'll come back to this
+    }
+    
+    @IBAction func playAgain(_ sender: Any) {
+        numSlider.setValue(50.0, animated: false)
+        randomNumber = Int(arc4random_uniform(101))
+        numLabel.text = String(randomNumber)
+        resultLabel.isHidden = true //must reappear next time it is checked ^
+        playAgainButton.isHidden = true
+        
+        if level != 1 && level > highScoreValue {
+            highScoreValue = level
+            highScore.text = String(highScoreValue)
+        }
+        
+        
+        range = 100
+        rangeLabel.text = String(range)
+        numSlider.maximumValue = Float(range)
+                
+        level = 1
+        currentLevel.text = String(level)
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        print(numSlider.value)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        randomNumber = Int(arc4random_uniform(101))
+        numLabel.text = String(randomNumber)
+        
+        rangeLabel.text = String(range)
+        currentLevel.text = String(level)
+    }
+
+}
+
