6131ca1949b76049557399783b9ef0fc65c64d82
Kevin Nguyen <kevinnguyen@berkeley.edu>
2022-02-20 14:26:41 -0800

    HW3 Halfway done

--- hw3/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
+++ hw3/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
@@ -7,15 +7,39 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct ContentView: View {
+    
+    @State var num: Double = 50
+    
+    var body: some View {
+        VStack() {
+            HStack() {
+                VStack() {
+                    Text("High Score")
+                    Text("0")
+                }
+                .padding(.leading, 20)
+                        
+                Spacer()
+                        
+                VStack() {
+                    Text("Current Level")
+                    Text("1")
+                }
+                .padding(.trailing, 20)
+            }
+                    
+            Spacer()
+            VStack() {
+                Text("Move the slider to: ").font(.system(size: 30.0))
+                Text(String(num)).font(.system(size: 30.0))
+                Slider(value: $num, in: 0...100)
+            }
+            Spacer()
+            Spacer()
+        }
+        .padding (.top, 20)
     }
 }
