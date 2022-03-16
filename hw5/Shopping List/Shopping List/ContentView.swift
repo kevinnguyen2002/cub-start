f68d61b6a546d93757128749da4a567d5113f3fd
Kevin Nguyen <kevinnguyen@berkeley.edu>
2022-03-16 16:29:18 -0700

    hw5: finished

--- hw5/Shopping List/Shopping List/ContentView.swift
+++ hw5/Shopping List/Shopping List/ContentView.swift
@@ -7,9 +7,66 @@
 
 import SwiftUI
 
+
+
+class Items: Identifiable {
+    
+    let imageName: String
+    let itemName: String
+    let quantity: Int
+    
+    init(imageName: String, itemName: String, quantity: Int) {
+        self.imageName = imageName
+        self.itemName = itemName
+        self.quantity = quantity
+    }
+    
+}
+
 struct ContentView: View {
     
+    var Fruits = [
+        Items(imageName: "banana", itemName: "Bananas", quantity: 3),
+        Items(imageName: "apple", itemName: "Apples", quantity: 4),
+        Items(imageName: "strawberry", itemName: "Strawberries", quantity: 12),
+        Items(imageName: "pineapple", itemName: "Pineapples", quantity: 2)
+    ]
+    
+    var Vegetables = [
+        Items(imageName: "lettuce", itemName: "Lettuce", quantity: 1),
+        Items(imageName: "kale", itemName: "Kale", quantity: 3),
+        Items(imageName: "potato", itemName: "Potatoes", quantity: 30),
+        Items(imageName: "green beans", itemName: "Green Beans", quantity: 1)
+    ]
+    
+    var Dairy = [
+        Items(imageName: "eggs", itemName: "Eggs", quantity: 12),
+        Items(imageName: "milk", itemName: "Milk", quantity: 2),
+        Items(imageName: "cheese", itemName: "Cheese", quantity: 0),
+        Items(imageName: "yogurt", itemName: "Yogurt", quantity: 100)
+    ]
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                Section(header: Text("Fruits")) {
+                    ForEach(Fruits) {
+                        i in CustomCell(imageName: i.imageName, itemName: i.itemName, quantity: i.quantity)
+                    }
+                }
+                
+                Section(header: Text("Vegetables")) {
+                    ForEach(Vegetables) {
+                        i in CustomCell(imageName: i.imageName, itemName: i.itemName, quantity: i.quantity)
+                    }
+                }
+                
+                Section(header: Text("Dairy")) {
+                    ForEach(Dairy) {
+                        i in CustomCell(imageName: i.imageName, itemName: i.itemName, quantity: i.quantity)
+                    }
+                }
+            }.navigationTitle("Shopping List")
+        }
     }
 }
--- hw5/Shopping List/Shopping List/CustomCell.swift
+++ hw5/Shopping List/Shopping List/CustomCell.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct CustomCell: View {
     
+    var imageName: String
+    
+    var itemName: String
+    
+    var quantity: Int
+    
+    init(imageName: String, itemName: String, quantity: Int) {
+        self.imageName = imageName
+        self.itemName = itemName
+        self.quantity = quantity
+    }
+    
     var body: some View {
-        Text("Hello World")
+        HStack {
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width:50, height:50)
+            
+                .padding(.trailing)
+            
+            Text(itemName)
+            
+            Spacer()
+            
+            Text(String(quantity))
+        }
     }
 }
 
